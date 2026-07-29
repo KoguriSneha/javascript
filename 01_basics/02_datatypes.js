f96082bc5a0af4e6b3f51a1ce53d58537aad7339
KoguriSneha <kogurisneha01@gmail.com>
2026-07-29 17:48:10 +0000

    container config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict";  //treat all JS code as newer version
+
+// alert(3+3)    // we are using nodejs, not browser
+console.log(3+3);
+console.log("Sneha");
+
+
+//-datatyp
+let name="Nikki"
+let age=18
+let isloggedIn=false
+let state;
+
+
+//num --2^53
+//bigint
+//string
+//boolean
+//null -- standalone value
+//undefined
+//symbol
+
+
+//object
+
+// console.log(typeof isloggedIn);
+// console.log(typeof "Nikki");
+console.log(typeof undefined);
