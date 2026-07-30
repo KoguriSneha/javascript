2f684ee34d784922bf214e9264cf8d804a2d6b66
KoguriSneha <kogurisneha01@gmail.com>
2026-07-30 04:15:55 +0000

    container config done

---
+++ 01_basics/03_Typeconver.js
@@ -0,0 +1,28 @@
+// let score="33"
+let score =null
+
+// const {score}=req.body
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+console.log(valueInNumber); //--if score=null -0 else NaN
+
+
+// "33"=33
+// "22abs"=NaN
+// true= 1;false=>0
+
+let isloggedIn= 1
+let booleanIsloggedIn=Boolean(isloggedIn)
+console.log(booleanIsloggedIn);
+
+// 1 = true , 0= false
+//  "" = false
+// "sneha"=true
+
+let somenum=33
+
+let stringnum=String(somenum)
+console.log(stringnum);
