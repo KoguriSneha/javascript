333a51012be2e196190666c728659a81062b2cb8
KoguriSneha <kogurisneha01@gmail.com>
2026-07-30 04:39:38 +0000

    container config done

--- 01_basics/03_Typeconver.js
+++ 01_basics/03_Typeconver.js
@@ -7,7 +7,7 @@ let score =null
 
 let valueInNumber = Number(score)
 // console.log(typeof valueInNumber);
-console.log(valueInNumber); //--if score=null -0 else NaN
+// console.log(valueInNumber); //--if score=null -0 else NaN
 
 
 // "33"=33
@@ -16,7 +16,7 @@ console.log(valueInNumber); //--if score=null -0 else NaN
 
 let isloggedIn= 1
 let booleanIsloggedIn=Boolean(isloggedIn)
-console.log(booleanIsloggedIn);
+// console.log(booleanIsloggedIn);
 
 // 1 = true , 0= false
 //  "" = false
@@ -25,4 +25,46 @@ console.log(booleanIsloggedIn);
 let somenum=33
 
 let stringnum=String(somenum)
-console.log(stringnum);
+// console.log(stringnum);
+
+
+// ****************************operations ==========================
+
+let value=3
+let neg=-value
+// console.log(neg);
+
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2-2);
+// console.log(3/2);
+// console.log(2%3);
+// console.log(2**2);
+
+
+let str1="hii"
+let str2="  Aishh"
+
+let str3=str1+str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+
+// console.log((3+4)*5%3);
+
+// console.log(+true); //1
+// console.log(+""); //0
+
+let num1,num2,num3
+num1=num2=num3=2+2
+
+let gamecounter=100
+// gamecounter++; //101
+++gamecounter;
+console.log(gamecounter);
+
